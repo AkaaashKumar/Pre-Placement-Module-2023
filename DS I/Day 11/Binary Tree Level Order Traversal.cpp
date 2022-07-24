393c39fc32f932cf968267eeebf4d52c4bf0cecc
AkaaashKumar <108126891+AkaaashKumar@users.noreply.github.com>
2022-07-23 21:14:10 -0400

    Create Binary Tree Level Order Traversal.cpp

---
+++ DS I/Day 11/Binary Tree Level Order Traversal.cpp
@@ -0,0 +1,18 @@
+ class Solution {
+public:
+    vector<vector<int> > levelOrder(TreeNode *root) {
+        vector<vector<int> > res;
+        if (root == NULL) return res;
+        
+        dfs(root, 1, res);
+        return res;
+    }
+    
+    void dfs(TreeNode *root, int dep, vector<vector<int> > &res) {
+        if (dep > res.size()) res.resize(dep);
+        res[dep-1].push_back(root->val);
+        
+        if (root->left != NULL)  dfs(root->left, dep+1, res);
+        if (root->right != NULL) dfs(root->right, dep+1, res);
+    }
+};
